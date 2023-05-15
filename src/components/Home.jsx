b8d4ef9fd21e5bd35d5c8b4663f240b4c055c258
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-05-15 01:24:49 -0400

    [Feature]: consuming API calls

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 
-import "./styles/navbar-styles.css"
-import "./styles/home-styles.css"
+import "./styles/navbar-styles.css";
+import "./styles/home-styles.css";
 
 export default function Home() {
-
   return (
-    
-      <p className="home-body">
-        <img src="https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274_1280.jpg"></img>
-      </p>
-    
-  )
-};
+    <p className="home-body">
+      <img src="https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274_1280.jpg"></img>
+    </p>
+  );
+}
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-import "./styles/navbar-styles.css"
+import "./styles/navbar-styles.css";
 
 export default function LandingPage() {
-
   return (
-    
-      <p className="landing-body">
-        <div> Landing page !</div>
-      </p>
-    
-  )
-};
+    <p className="landing-body">
+      <div> Landing page !</div>
+    </p>
+  );
+}
