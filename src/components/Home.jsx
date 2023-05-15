8ae9229fe69fdc31ba6ea1d5eec8d047bf0d3ffe
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-05-15 11:43:03 -0400

    [feature]: Home page UI cahnges and API consumption

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Button from "../components/Button";
 
 import "./styles/navbar-styles.css";
 import "./styles/home-styles.css";
+import spaceship from "./images/space_ship.gif";
 
 export default function Home() {
+  const handleButtonClick = () => {
+    console.log("Button clicked");
+  };
+
   return (
-    <p className="home-body">
-      <img src="https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274_1280.jpg"></img>
-    </p>
+    <div className="home-body">
+      <h1>Wlecome to Hotel Cosmic </h1>
+      <div className="button-container">
+        <h3>Choose the options below</h3>
+        <Link to="/landing">
+          <Button text="Experience Tour" />
+        </Link>
+        <Link to="/reservations">
+          <Button text="Room Reservations" />
+        </Link>
+      </div>
+    </div>
   );
 }
