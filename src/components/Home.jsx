533c2ec73b7dc9ffbede6c448b21f0a1d4228fef
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-05-15 13:29:37 -0400

    [Feature] : Addition of home page and button component with its styles.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,18 +5,17 @@ import Button from "../components/Button";
 
 import "./styles/navbar-styles.css";
 import "./styles/home-styles.css";
+import "./styles/button-style.css";
 import spaceship from "./images/space_ship.gif";
 
 export default function Home() {
-  const handleButtonClick = () => {
-    console.log("Button clicked");
-  };
-
   return (
     <div className="home-body">
-      <h1>Wlecome to Hotel Cosmic </h1>
-      <div className="button-container">
+      <h1>Welcome to Hotel Cosmic</h1>
+      <div>
         <h3>Choose the options below</h3>
+      </div>
+      <div className="button-container">
         <Link to="/landing">
           <Button text="Experience Tour" />
         </Link>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,29 +1,11 @@
 import React from "react";
 
-import { useCustomers } from "../hooks/useEffect";
-
 import "./styles/navbar-styles.css";
 
 export default function LandingPage() {
-  const customers = useCustomers();
-  console.log("******", typeof customers);
-  console.log("******", customers);
   return (
     <p className="landing-body">
       <div> Landing page !</div>
-      <div className="landing-body">
-        <h2> *** Customers ****</h2>
-        <div className="customer-list">
-          {Array.isArray(customers.customers) &&
-            customers.customers.map((customer) => (
-              <div key={customer.id}>
-                <p>Name: {customer.name}</p>
-                <p>Email: {customer.email}</p>
-                <p>ID: {customer.id}</p>
-              </div>
-            ))}
-        </div>
-      </div>
     </p>
   );
 }
