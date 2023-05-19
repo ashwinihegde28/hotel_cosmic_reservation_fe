bb9fa783c9ff1fec55f1222240a0c19a8edf9d0b
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-05-19 11:11:45 -0400

    [Integration]: invoices to reservation

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -11,17 +11,18 @@ export default function InvoicePopup({ invoiceData, setShowInvoice }) {
   const handleEmail = () => {
     // Stretch : Logic to handle emailing the invoice
   };
-
   const descriptionValues = invoiceData.description.split(",");
 
   return (
     <div className="invoice-popup">
+      <h1>Invoices</h1>
       <h4>Invoice Number: {invoiceData.invoiceNumber}</h4>
       <div className="invoice-details">
         <h5>Invoice Details</h5>
         <p>Name of Customer: {descriptionValues[0]}</p>
         <p>Email ID: {descriptionValues[1]}</p>
         <p>Room Type: {descriptionValues[2]}</p>
+        {/* <p>Room Type: {descriptionValues[3]}</p> */}
       </div>
       <div className="invoice-options">
         <Button text="Close" onClick={() => setShowInvoice(false)} />
