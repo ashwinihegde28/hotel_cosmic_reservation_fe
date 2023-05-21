cba02ad4f0172789aa579a8e2d1f45a3218c3f81
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-05-21 01:31:23 -0400

    [css fix]: modified the invoice pop up and its css

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -32,10 +32,10 @@ export default function InvoicePopup({ invoiceData, setShowInvoice }) {
       <h4>Invoice Number: {invoiceData.id}</h4>
       <div className="invoice-details">
         <h5>Invoice Details</h5>
-        <p>Name of Customer: {descriptionValues[0]}</p>
-        <p>Email ID: {descriptionValues[1]}</p>
-        <p>Room Number: {descriptionValues[2]}</p>
-        <p>Total Amount: {descriptionValues[3]} Digital currencies</p>
+        <p>Name : {descriptionValues[0]}</p>
+        <p>Email ID : {descriptionValues[1]}</p>
+        <p>Room Number : {descriptionValues[2]}</p>
+        <p>Total Amount : {descriptionValues[3]} Digital currencies</p>
 
         {/* <p>Room Type: {descriptionValues[3]}</p> */}
       </div>
