469f2d89938b3cc5b531a3c3bcbdfa9fde49a416
Catherine Mitchell <nicholscat@yahoo.com>
2023-05-21 18:07:04 -0300

    added scroll feature to scroll to top of invoice when the book now button is clicked

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
 import Button from "../components/Button";
 
 import "./styles/invoice-popup-style.css";
@@ -9,13 +9,25 @@ export default function InvoicePopup({ invoiceData, setShowInvoice }) {
     window.print();
   };
 
+  const location = useLocation();
+  const invoiceRef = useRef(null);
+
+  useEffect(() => {
+    if (invoiceRef.current) {
+      window.scrollTo({
+        top: invoiceRef.current.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  }, [location]);
+
   const handleEmail = () => {
     // Stretch : Logic to handle emailing the invoice
   };
   const descriptionValues = invoiceData.description.split(",");
 
   return (
-    <div className="invoice-popup">
+    <div className="invoice-popup" >
       <h1>Invoice</h1>
       <h4>Invoice Number: {invoiceData.id}</h4>
       <div className="invoice-details">
@@ -27,7 +39,7 @@ export default function InvoicePopup({ invoiceData, setShowInvoice }) {
 
         {/* <p>Room Type: {descriptionValues[3]}</p> */}
       </div>
-      <div className="invoice-options">
+      <div className="invoice-options" ref={invoiceRef}>
         <Link to="/">
           <Button text="Close" onClick={() => setShowInvoice(false)} />
         </Link>
